db7e3d74946d1f7807d41e51952ca54b8c3aa4c5
subhroacharjee <sacharjeeofficial@gmail.com>
2023-05-03 11:56:20 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,17 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const webhook = core.getInput('webook', {
-      required: false
-    })
+    // const webhook = core.getInput('webook', {
+    //   required: false
+    // })
 
-    const token = core.getInput('token', {
-      required: false
-    })
+    // const token = core.getInput('token', {
+    //   required: false
+    // })
 
     const event = JSON.parse(core.getInput('event'))
-    core.info([webhook, token].join(' '))
-
-    const context = github.context
-    core.info(context.eventName)
-    core.info(event.action)
+    core.info(JSON.stringify(event))
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
